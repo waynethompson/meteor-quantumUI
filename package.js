f3ed334277b7d7e71a9da06c63eb6425bef83287
AMOS <amos@AMOSs-Mac-mini.local>
2016-01-19 22:12:04 +0700

    Added description

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'waynethompson:quantum-ui',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'A package for the QuantumUI component library',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/waynethompson/meteor-quantumUI.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
